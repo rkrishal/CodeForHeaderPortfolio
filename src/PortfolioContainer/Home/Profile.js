2934ed468c9508207dfbff6ce0e87f5fa20b4e90
rkrishal <mdrishal11@gmail.com>
2022-07-29 01:05:37 +0530

    3 files modified, 1 added

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typical from 'react-typical';
+import './Profile.css';
 
 export default function Profile() {
   return (
@@ -7,19 +8,21 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.facebook.com/rk.rishal/">
-              <i className="fa fa-facebook-square"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/rk.rishal/">
+                <i className="fa fa-facebook-square"></i>
+              </a>
 
-            <a href="https://www.instagram.com/rk_rishal/">
-              <i className="fa fa-instagram"></i>
-            </a>
-            <a href="https://github.com/rkrishal">
-              <i className="fa fa-github-square"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/rkrishal/">
-              <i className="fa fa-linkedin-square"></i>
-            </a>
+              <a href="https://www.instagram.com/rk_rishal/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://github.com/rkrishal">
+                <i className="fa fa-github-square"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/rkrishal/">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+            </div>
           </div>
 
           <div className="profile-details-name">
@@ -52,19 +55,23 @@ export default function Profile() {
               </span>
             </span>
           </div>
-        <div className="profile-option">
-        <button className="btn primary-btn">
-          {""}
-          Hire Me{" "}
-          </button>
-          <a href="https://cdn.jsdelivr.net/gh/rkrishal/RK-Portfolio@main/asset/M_Mohamed_Rishal__CV_-2.pdf" download="Mohamed Rishal.pdf" >
-            <button className="btn highlighted-btn">Get Resume
+          <div className="profile-option">
+            <button className="btn primary-btn">
+              {''}
+              Hire Me{' '}
             </button>
+            <a
+              href="https://cdn.jsdelivr.net/gh/rkrishal/RK-Portfolio@main/asset/M_Mohamed_Rishal__CV_-2.pdf"
+              download="Mohamed Rishal.pdf"
+            >
+              <button className="btn highlighted-btn">Get Resume</button>
             </a>
-            </div>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
-  
